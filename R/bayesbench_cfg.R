1dce0200fc4255ba7b9535b512562461a409d6cf
Måns Magnusson <mons.magnusson@gmail.com>
2019-04-11 12:12:43 +0300

    Added handling of job config

--- R/bayesbench_cfg.R
+++ R/bayesbench_cfg.R
@@ -7,12 +7,12 @@
 #' @param file_path File path to bayesbench configuration file.
 #' 
 #' @examples 
-#' path <- system.file("extdata", "examples", "test_8schools_advi.yml", package = "bayesbenchr")
-#' x <- read_bayesbench_cfg_from_file(path)
+#' file_path <- system.file("extdata", "examples", "test_8schools_advi.yml", package = "bayesbenchr")
+#' x <- read_bayesbench_cfg_from_file(file_path)
 #' 
 #' @export
 bayesbench_cfg <- function(...){
-  bayesbench_cfg_from_list(list(inference_engine, posterior_name, ...))
+  bayesbench_cfg_from_list(...)
 }
 
 #' @rdname bayesbench_cfg
@@ -91,3 +91,60 @@ print.bayesbench_cfg <- function(x,...){
   cat(bayesbench_cfg_toYAML(x))
 }
 
+
+
+#' Expand \code{bayesbench_cfg} objects to \code{bayesbench_job_cfg}s
+#' 
+#' @details job configs are individual configurations that can be run independently
+#' 
+#' @param cfg a list of \code{bayesbench_cfg} to expand.
+#' 
+expand_bayesbench_cfg_to_job_cfgs <- function(cfg){
+  checkmate::assert_list(cfg)
+  for(i in seq_along(cfg)){
+    checkmate::assert_class(cfg[[i]], "bayesbench_cfg")
+  }
+  all_jobs <- list()
+  for(i in seq_along(cfg)){
+    all_jobs <- c(all_jobs, bayesbench_job_cfg_from_cfg(cfg[[i]]))
+  }
+  all_jobs
+}
+
+bayesbench_job_cfg_from_cfg <- function(x){
+  checkmate::assert_class(x, "bayesbench_cfg")
+  expand_list <- list()
+  expand_list[["inference_engine"]] <- x$inference_engine
+  expand_list[["posterior_name"]] <- x$posterior_name
+  for(i in seq_along(x$method_specific_arguments)){
+    if(length(x$method_specific_arguments[[i]]) > 1) expand_list[[names(x$method_specific_arguments)[i]]] <- x$method_specific_arguments[[i]]
+  }
+  expand_df <- expand.grid(expand_list, stringsAsFactors = FALSE)
+  if(nrow(expand_df) == 1) {
+    return(list(bayesbench_job_cfg(x)))
+  }
+  job_cfg_list <- list()
+  for(i in 1:nrow(expand_df)){
+    job_cfg_list[[i]] <- x
+    job_cfg_list[[i]]$inference_engine <- expand_df$inference_engine[i]
+    job_cfg_list[[i]]$posterior_name <- expand_df$posterior_name[i]
+    for(j in 3:ncol(expand_df)){
+      job_cfg_list[[i]]$method_specific_arguments[[names(expand_df)[j]]] <- expand_df[i,j]
+    }
+    job_cfg_list[[i]] <- bayesbench_job_cfg(job_cfg_list[[i]])
+  }
+  return(job_cfg_list)
+}
+
+
+bayesbench_job_cfg <- function(x){
+  checkmate::assert_class(x, "bayesbench_cfg")
+  x <- bayesbench_cfg(x)
+  checkmate::assert_string(x$inference_engine)
+  checkmate::assert_string(x$posterior_name)
+  for(i in seq_along(x$method_specific_arguments)){
+    checkmate::assert_true(length(x$method_specific_arguments[[i]]) == 1L)
+  }
+  class(x) <- c("bayesbench_job_cfg", class(x))
+  x
+}
